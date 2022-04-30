a9a925ae6f60c3861d8e8980139b791b6e6cb727
Jayesh Choudhary <jayeshchoudhary@Jayeshs-MacBook-Pro.local>
2022-05-01 02:28:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
-
+import { Switch, Route, Redirect } from 'react-router-dom';
+import AdminLoginPage from './features/admin/components/login';
+import AdminAllBookings from './features/admin/components';
+import RetailLoginPage from './features/retail/components/login';
+import RetailSignUpPage from './features/retail/components/signup';
+import RetailHomePage from './features/retail/components'
+import NavBar from './navbar';
+import { useSelector } from 'react-redux';
+import { selectUserActivity } from './features/retail/slice';
+import { RetailSnackBar } from './components/retail-snackbar';
+import { selectAdminActivity } from './features/admin/slice';
+function PrivateRoute({ children, ...rest }) {
+  const userActivity = useSelector(selectUserActivity);
+  const adminActivity = useSelector(selectAdminActivity);
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>{
+        if(userActivity.loggedIn || adminActivity.adminLoggedIn){
+          return children;
+        }
+        if(!userActivity.loggedIn){
+          return <Redirect to={{
+            pathname: '/login',
+            state: { from: location }
+          }} />
+        }
+        if(!adminActivity.adminLoggedIn){
+          return <Redirect to={{
+            pathname: '/admin/login/',
+            state: { from: location }
+          }} />
+        }
+      }
+      }
+    />
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <NavBar/>
+      <Switch>
+        <PrivateRoute  exact path="/">
+          <RetailHomePage />
+        </PrivateRoute>
+        <Route path="/login/">
+          <RetailLoginPage  />
+        </Route>
+        <Route path="/signup/" >
+          <RetailSignUpPage />
+        </Route>
+        <Route path="/admin/login/">
+          <AdminLoginPage />
+        </Route>
+        <PrivateRoute exact path="/admin/">
+          <AdminAllBookings />
+        </PrivateRoute>
+      </Switch>
+      <RetailSnackBar/>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
+import { store, persister } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+import { PersistGate } from 'redux-persist/integration/react'
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <Provider store={store}>
+      <PersistGate loading={null} persistor={persister}>
+    <Router>
+        <App />
+    </Router>
+    </PersistGate>
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
